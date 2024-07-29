b87591ca3e31c6536a73d173a1066aa6ab1d7b60
LariceG <larice.zcc@gmail.com>
2024-07-29 10:29:34 +0530

    update changes

--- app/Mail/RjaMail.php
+++ app/Mail/RjaMail.php
@@ -20,24 +20,20 @@ class RjaMail extends Mailable
 
     public function build()
     {
-<<<<<<< HEAD
-        dd($this->rja->companies);
-=======
-        
+
         $subject = 'RJA Details for ' . $this->rja->b2b_reference;
 
-        if($this->rja->status == Rja::STATUS_APPROVED){
+        if ($this->rja->status == Rja::STATUS_APPROVED) {
             $subject = 'RJA Approved Successfully ' . $this->rja->b2b_reference;
-        }elseif($this->rja->status == Rja::STATUS_REJECTED){
+        } elseif ($this->rja->status == Rja::STATUS_REJECTED) {
             $subject = 'RJA Rejected Successfully ' . $this->rja->b2b_reference;
         }
         //dd('RJA Details for ' . $this->rja->b2b_reference);
->>>>>>> master
         return $this->from(config('mail.from.address'), config('mail.from.name'))
             ->subject($subject)
-        ->view('emails.rja')
-        ->with([
-            'rja' => $this->rja,
-        ]);
+            ->view('emails.rja')
+            ->with([
+                'rja' => $this->rja,
+            ]);
     }
 }
