5c2937c9285b66fa86712482af2021010b3b8a40
LariceG <larice.zcc@gmail.com>
2024-07-30 11:27:01 +0530

    fixed routes

--- resources/views/rja/success.blade.php
+++ resources/views/rja/success.blade.php
@@ -5,7 +5,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Rja Error</title>
+  <title>Rja Success</title>
   <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
 </head>
 
